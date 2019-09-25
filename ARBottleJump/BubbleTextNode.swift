2499da43c825beacc7886f098ca143ff7821afec
Winston Zhang <winston.zhangwentong@gmail.com>
2019-09-25 18:05:59 +0800

    https://github.com/Mosquito1123/BottleJumper.git

--- ARBottleJump/BubbleTextNode.swift
+++ ARBottleJump/BubbleTextNode.swift
@@ -77,7 +77,7 @@ class BubbleTextNode: SCNNode {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
 }
